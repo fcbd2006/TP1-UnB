db31015a7c5e1708df9e39952afb9f8dacde4023
Daniel <danielbgs10@gmail.com>
2026-05-02 20:17:38 -0300

    Adição dos códigos de Email, Senha e Texto.

--- Code/Domains/dominios.cpp
+++ Code/Domains/dominios.cpp
@@ -1,13 +1,14 @@
 #include <Domains/Headers/outros_dominios.hpp>
 #include <stdexcept>
+#include <cctype>
 
 // CODIGO
 void Codigo::validar(const std::string& codigo){
     for(int index=0;index<=1;index++){
-        if(codigo[index]<'A' or codigo[index]>'Z') throw std::invalid_argument("Valor Inválido para Código.");
+        if(codigo[index]<'A' || codigo[index]>'Z') throw std::invalid_argument("Valor Inválido para Código.");
     }    
     for(int index=2;index<=4;index++){
-        if(codigo[index]<'0' or codigo[index]>'9') throw std::invalid_argument("Valor Inválido para Código.");
+        if(codigo[index]<'0' || codigo[index]>'9') throw std::invalid_argument("Valor Inválido para Código.");
     }
 }
 ////////////////////////////////////////////////////////////////
@@ -15,7 +16,7 @@ void Codigo::validar(const std::string& codigo){
 // DATA
 void Data::validar(const std::string& data){
     if(data.size()!=10) throw std::invalid_argument("Valor Inválido para Data.");
-    if(data[2]!='/' or data[5]!='/') throw std::invalid_argument("Valor Inválido para Data.");
+    if(data[2]!='/' || data[5]!='/') throw std::invalid_argument("Valor Inválido para Data.");
     for(int index=0;index<= int (data.size());index++){
         if(index==2||index==5) continue;
         if(data[index]<'0' || data[index]>'9') throw std::invalid_argument("Valor Inválido para Data.");
@@ -24,18 +25,59 @@ void Data::validar(const std::string& data){
     int mes = stoi(data.substr(3,2));
     int ano = stoi(data.substr(6,4));
 
-    if(mes>12 or mes<1) throw std::invalid_argument("Valor Inválido para Data.");
-    if(ano>2999 or ano<2000) throw std::invalid_argument("Valor Inválido para Data.");
+    if(mes>12 || mes<1) throw std::invalid_argument("Valor Inválido para Data.");
+    if(ano>2999 || ano<2000) throw std::invalid_argument("Valor Inválido para Data.");
 
     int diasMensais[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
-    if(mes==2 and ehBisexto(ano)) diasMensais[2]=29;
+    if(mes==2 && ehBisexto(ano)) diasMensais[2]=29;
     
-    if(!(dia>0 and dia<=diasMensais[mes])) throw std::invalid_argument("Valor Inválido para Data.");
+    if(!(dia>0 && dia<=diasMensais[mes])) throw std::invalid_argument("Valor Inválido para Data.");
 }
 ////////////////////////////////////////////////////////////////
 
 // EMAIL
+void Email::validar(const std::string& email){
+    //Encontrar a posição do "@" e garantir que não está nos extremos.
+    size_t position = email.find('@');
+    if(position == std::string::npos || position == 0 || position == email.length() -1){
+        throw std::invalid_argument("Valor Inválido para Email.");
+    }
+
+    std::string local = email.substr(0, position);
+    std::string dominio = email.substr(position + 1);
+
+    //Conferir comprimentos máximos.
+    if(local.length() > 64 || dominio.length() > 255){
+        throw std::invalid_argument("Valor Inválido para Email.");
+    }
+
+    //Função para validar regras de caracteres e pontuação.
+    auto validar_formato = [](const std::string& parte){
+        if(parte.front() == '.' || parte.front() == '-' || parte.back() == '.' || parte.back() == '-'){
+            throw std::invalid_argument("Valor Inválido para  Email.");
+        }
+
+        for(size_t i = 0; i < parte.length(); i++){
+            char c = parte[i];
+
+            if(!isalnum(c) && c != '.' && c != '-'){
+                throw std::invalid_argument("Valor Inválido para Email.");
+            }
+
+            if((c == '.' || c == '-') && i + 1 < parte.length()){
+                char proximo = parte[i + 1];
+                if(proximo == '.' || proximo == '-'){
+                    throw std::invalid_argument("Valor Inválido para Email.");
+                }
+            }
+        }
+    };
+    
+    //validação das partes do email por meio da função.
+    validar_formato(local);
+    validar_formato(dominio);
+}
 
 ////////////////////////////////////////////////////////////////
 
@@ -52,10 +94,10 @@ void Nome::validar(const std::string& nome){
     if(nome[0]==' ' || nome[nome.size()-1]==' ') throw std::invalid_argument("Valor Inválido para Nome.");
     for(int index=0;index<=(int(nome.size())-1);index++){
 
-        if(nome[index]==' ' and ((nome[index+1]<'A' || nome[index+1]>'Z') and (nome[index+1]<'a' || nome[index+1]>'z')))
+        if(nome[index]==' ' && ((nome[index+1]<'A' || nome[index+1]>'Z') && (nome[index+1]<'a' || nome[index+1]>'z')))
             throw std::invalid_argument("Valor Inválido para Nome.");
     
-        if((nome[index]<'A' || nome[index]>'Z') and (nome[index]<'a' || nome[index]>'z') and nome[index]!=' ')
+        if((nome[index]<'A' || nome[index]>'Z') && (nome[index]<'a' || nome[index]>'z') && nome[index]!=' ')
             throw std::invalid_argument("Valor Inválido para Nome.");
     }
 }
@@ -64,19 +106,61 @@ void Nome::validar(const std::string& nome){
 
 // PAPEL
 void Papel::validar(const std::string& papel){
-    if(!(papel=="DESENVOLVEDOR" or papel=="MESTRE SCRUM" or papel=="PROPRIETARIO DE PRODUTO")) throw std::invalid_argument("Valor Inválido para Papel.");
+    if(!(papel=="DESENVOLVEDOR" || papel=="MESTRE SCRUM" || papel=="PROPRIETARIO DE PRODUTO")) throw std::invalid_argument("Valor Inválido para Papel.");
 }
 
 ////////////////////////////////////////////////////////////////
 
 // PRIORIDADE
 void Prioridade::validar(const std::string& prioridade){
-    if(!(prioridade=="ALTA" or prioridade=="MEDIA" or prioridade=="BAIXA")) throw std::invalid_argument("Valor Inválido para Prioridade.");
+    if(!(prioridade=="ALTA" || prioridade=="MEDIA" || prioridade=="BAIXA")) throw std::invalid_argument("Valor Inválido para Prioridade.");
 }
 
 ////////////////////////////////////////////////////////////////
 
 // SENHA
+void Senha::validar(const std::string& senha){
+    //verificar tamanho.
+    if(senha.length() != 6){
+        throw std::invalid_argument("Valor Inválido para Senha.");
+    }
+
+    bool tem_maiuscula = false;
+    bool tem_minuscula = false;
+    bool tem_digito = false;
+
+    //verificar regras de formatação da senha.
+    for(size_t i = 0; i < senha.length(); i++){
+        char c = senha[i];
+
+        if(isupper(c)){
+            tem_maiuscula = true;
+        }
+        else if(islower(c)){
+            tem_minuscula = true;
+        }
+        else if(isdigit(c)){
+            tem_digito = true;
+        }
+        else{
+            throw std::invalid_argument("Valor Inválido para Senha.");
+        }
+        //garante a não repetição de caracteres.
+        if(senha.find(c, i+1) != std::string::npos){
+            throw std::invalid_argument("Valor Inválido para Senha.");
+        }
+
+        if(i > 0){
+            if((isalpha(senha[i - 1]) && isalpha(c)) || (isdigit(senha[i - 1]) && isdigit(c))){
+                throw std::invalid_argument("Valor Inválido para Senha.");
+            }
+        }
+    }
+
+    if(!tem_maiuscula || !tem_minuscula || !tem_digito){
+        throw std::invalid_argument("Valor Inválido para Senha.");
+    }
+}
 
 ////////////////////////////////////////////////////////////////
 
@@ -92,5 +176,35 @@ void Tempo::validar(const std::string& tempo){
 ////////////////////////////////////////////////////////////////
 
 // TEXTO
+void Texto::validar(const std::string& texto){
+    //verificar comprimento e se é vazio.
+    if(texto.empty() || texto.length() > 40){
+        throw std::invalid_argument("Valor Inválido para Texto.");
+    }
+    //verificar o primeiro caractere.
+    if(texto.front() == ' ' || texto.front() == ',' || texto.front() == '.'){
+        throw std::invalid_argument("Valor Inválido para Texto.");
+    }
+    //verificar o último caractere.
+    if(texto.back() == ',' || texto.back() == '.'){
+        throw std::invalid_argument("Valor Inválido para Texto.");
+    }
+    //verificar caracteres e duplicatas.
+    for(size_t i = 0; i < texto.length(); i++){
+        char c = texto[i];
+
+        if(!isalnum(c) && c != ' ' && c != ',' && c != '.'){
+            throw std::invalid_argument("Valor Inválido para Texto.");
+        }
+
+        if(i > 0){
+            char anterior = texto[i - 1];
+            if((c == ' ' || c == ',' || c == '.') && (anterior == ' ' || anterior == ',' || anterior == '.')){
+                throw std::invalid_argument("Valor Inválido para Texto.");
+            }
+        }
+    }
+
+}
 
 ////////////////////////////////////////////////////////////////
