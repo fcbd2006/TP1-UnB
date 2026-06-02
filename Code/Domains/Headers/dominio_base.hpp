9a5a72b3076adfcb744adff7c1abd33052bc4ea1
Daniel <danielbgs10@gmail.com>
2026-06-02 11:31:29 -0300

    Comentários da classe Dominio para o Doxygen

--- Code/Domains/Headers/dominio_base.hpp
+++ Code/Domains/Headers/dominio_base.hpp
@@ -1,18 +1,49 @@
 #ifndef DOMINIO_HPP_INCLUDED
 #define DOMINIO_HPP_INCLUDED
-
 #include <string>
 
-// Declaração de classe base para padronização dos domínios
+/**
+ * @class Dominio
+ * @brief Classe base abstrata para padronização dos domínios.
+ * * Esta classe fornece a estrutura fundamental para armazenar um valor de domínio. 
+ * As classes derivadas devem obrigatoriamente implementar a lógica específica 
+ * de validação através do método validar().
+ */
 class Dominio {
     private:
+        /**
+         * @brief Atributo que armazena o valor do domínio.
+         */
         std::string valor;
+
+        /**
+         * @brief Método virtual puro para validação do valor.
+         ** Este método é chamado automaticamente pelo método setValor().
+         * Classes filhas devem implementá-lo, tipicamente lançando uma exceção (ex: std::invalid_argument)
+         * caso a validação falhe.
+         */
         virtual void validar(const std::string&) = 0;
 
     public:
+        /**
+         * @brief Construtor padrão da classe domínio. 
+        */
         Dominio() = default;
+
+        /** 
+         * @brief Destrutor virtual padrão da classe domínio.
+         * Garante a destruição correta das classes derivadas.
+        */
         virtual ~Dominio() = default;
+
+        /** 
+         * @brief Método para definir o valor do domínio.
+        */ 
         void setValor(const std::string&);
+
+        /** @brief Método para obter o valor do domínio.
+         * @return Retorna uma referência constante para a string contendo o valor do domínio.
+         */
         const std::string& getValor() const;
 };
 
@@ -24,7 +55,7 @@ inline const std::string& Dominio::getValor() const{
 // Método para armazenar um valor
 inline void Dominio::setValor(const std::string& value){
     validar(value);
-    valor=value;
+    valor = value;
 }
 
 #endif
